c15c219e9e5ed3332e5093e3d31a91229016c2ec
nirvanafan205 <nirvanafan205@gmail.com>
2022-11-12 20:42:41 -0800

    appending node

--- C++/Linkedlist.cpp
+++ C++/Linkedlist.cpp
@@ -25,6 +25,41 @@ void push(Node** head_ref, int new_data)
 	(*head_ref) = new_node;
 }
 
+void append(Node** head_ref, int new_data)
+{
+	//allocate node
+	Node* new_node = new Node();
+
+	Node *last  = *head_ref;
+
+	//assign data
+	new_node->data = new_data;
+
+	//new node is made to be last node of the list
+	//must be NULL to be able to stop traversal
+	new_node->next = NULL;
+
+	//checks if LL is empty
+	//makes the new node as head
+	
+	if(*head_ref == NULL)
+	{
+		*head_ref = new_node;
+		return;
+	}
+
+	//traverse until the last node
+	
+	while(last->next != NULL)
+	{
+		last = last->next;
+	}
+
+	//change the next of the last node
+	last->next = new_node;
+	return;
+}
+
 void printList(Node *node)
 {
 	while(node != NULL)
@@ -38,11 +73,17 @@ int main()
 {
 	Node* head = NULL;
 
+	//adds at the head of LL
 	push(&head, 7);
 	push(&head, 1);
 	push(&head, 9);
+	//9 1 7 
+	
+	//add's at the tail LL
+	append(&head, 5);
+	append(&head, 2);
+	append(&head, 9);
+	//9 1 7 5 2 9
 
 	printList(head);
-
-
 }
